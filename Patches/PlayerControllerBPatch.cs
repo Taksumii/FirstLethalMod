8c941f20a2c029d599b7e451af9a3752937ec724
Taksumiiii <111644113+Taksumii@users.noreply.github.com>
2023-12-14 04:25:13 +0000

    Inf sprint
    Inf sprint mod

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace ModTobe.Patches
+{
+    [HarmonyLib.HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void InfiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+
+        }
+
+        
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,28 @@
 ﻿using BepInEx;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System;
 
 namespace ModTobe
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    public class ControllerPatch : BaseUnityPlugin
     {
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+
+            Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+            harmony.PatchAll(typeof(ControllerPatch));
+            harmony.PatchAll(typeof(Patches.PlayerControllerBPatch));
+            
+
         }
     }
+
+    
 }
 
