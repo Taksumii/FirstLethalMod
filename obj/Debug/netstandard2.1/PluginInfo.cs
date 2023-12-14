7156a3d5a656429b8c3dd57b5b7466611bfaabab
Taksumiiii <111644113+Taksumii@users.noreply.github.com>
2023-12-14 02:49:17 +0000

    Initial commit

---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ModTobe
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ModTobe";
+        public const string PLUGIN_NAME = "ModTobe";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
